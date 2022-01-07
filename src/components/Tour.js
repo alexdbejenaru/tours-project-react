e5faec3d75f6be35dc46e96088c2ebebd6546b9e
Alex Bejenaru <alex.d.bejenaru@gmail.com>
2022-01-07 16:04:50 +0200

    +1

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,8 +1,11 @@
-const Tour = () => {
+const Tour = ({ index, tour }) => {
     return ( 
-        <>
-        
-        </>
+        <div key={index} className="tour">
+            <h1 className="tour_title">{tour.name}</h1>
+            <img className="tour_image" src={tour.image} alt={tour.name} />
+            <p className="tour_text">{tour.info}</p>
+            <p className="tour_price">{tour.price}<span>$</span></p>
+        </div>
      );
 }
  
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,8 +1,13 @@
-const Tours = ({ data, loading, error }) => {
+import Tour from "./Tour";
+
+const Tours = ({ data }) => {
+    const tours = data;
     return ( 
         <>
             <div className="tours-container">
-
+                {tours.map((tour, index) => (
+                    <Tour index={ index } tour={ tour }/>
+                ))}
             </div>
         </>
      );
